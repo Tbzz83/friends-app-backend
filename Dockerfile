0f1235fa846e6c6187ea182be9ee65effd5be4e0
Tobi Azeez <azeezoe@appstate.edu>
2024-12-08 12:54:58 -0500

    Containerizing backend

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Use an official Python runtime as a parent image
+FROM python:3.11-slim
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port the app will run on
+EXPOSE 5000
+
+# Set environment variables
+ENV FLASK_APP=app.py
+ENV FLASK_RUN_HOST=0.0.0.0
+# Change below to 'production' in production environment
+ENV FLASK_ENV=development  
+
+# Run the Flask app
+CMD ["flask", "run"]
+
--- app.py
+++ app.py
@@ -14,16 +14,19 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-frontend_folder = os.path.join(os.getcwd(),"..","frontend")
-dist_folder = os.path.join(frontend_folder,"dist")
+#frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+#dist_folder = os.path.join(frontend_folder,"dist")
 
 # Server static files from the "dist" folder under the "frontend" directory
-@app.route("/",defaults={"filename":""})
-@app.route("/<path:filename>")
-def index(filename):
-  if not filename:
-    filename = "index.html"
-  return send_from_directory(dist_folder,filename)
+#@app.route("/",defaults={"filename":""})
+#@app.route("/<path:filename>")
+#def index(filename):
+#  if not filename:
+#    filename = "index.html"
+#  return send_from_directory(dist_folder,filename)
+
+# Optional: Enable CORS if frontend and backend are on different domains
+CORS(app)
 
 # api routes
 import routes
@@ -32,4 +35,7 @@ with app.app_context():
   db.create_all()
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  host = os.getenv("FLASK_RUN_HOST", "0.0.0.0")
+  port = int(os.getenv("FLASK_RUN_PORT", 5000))
+
+  app.run(debug=True, host=host, port=port)
